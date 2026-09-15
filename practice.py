5fa9f73e38211387d9484cb5a662c71f24634971
madhav <madhavmongaa@gmail.com>
2026-09-15 16:28:55 +0530

    help

--- practice.py
+++
@@ -1,5 +0,0 @@
-# or and def function practice
-tit = input("enter a title: ") or "Mr"
-first_name = input("enter your first name: ") or "Zen"
-last_name = input("enter your last name: ") or "G"
-print(tit, first_name, last_name)
