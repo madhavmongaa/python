f37881a26af24cc862051f143a7f2325b12f531e
madhav <madhavmongaa@gmail.com>
2026-09-16 00:27:08 +0530

    tuff

---
+++ visiting_card.py
@@ -0,0 +1,13 @@
+#User Inputs
+name = input("Enter your Name: ")
+city = input("Enter your City: ")
+hobby = input("Enter your Hobby: ")
+
+#Digital Visiting Card
+print("\n--------------------------------------")
+print("|       DIGITAL VISITING CARD        |")
+print("--------------------------------------")
+print(f"| Name:  {name:<27} |")
+print(f"| City:  {city:<27} |")
+print(f"| Hobby: {hobby:<27} |") 
+print("--------------------------------------")
